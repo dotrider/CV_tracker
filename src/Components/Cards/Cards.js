a6977a27e6f877572b44da1d493b3c4d0f2d066d
dotrider <56666461+dotrider@users.noreply.github.com>
2020-06-02 21:11:47 -0700

    info cards set with styles

--- src/Components/Cards/Cards.js
+++ src/Components/Cards/Cards.js
@@ -3,26 +3,83 @@ import React from 'react';
 import { Card, CardContent, Typography, Grid } from '@material-ui/core';
 //importing styles from =>
 import styles from './Cards.module.css'
+//couter for digits
+import CountUp from 'react-countup'
+//classnames for styles => allows to use mulitple class from the styles import and use them in the className prop as a method cx()
+import cx from 'classnames'
 
-const Cards = (props) => {
 
-    console.log('cards', props)
+const Cards = ({data: {confirmed, recovered, deaths, lastUpdate}}) => {
+
+if(!confirmed)return 'Loading..'
+
+    console.log('cards', confirmed, recovered, deaths, lastUpdate)
     return(
         <div className={styles.container}>
             <Grid container spacing={3} justify='center'>
-                <Grid item component={Card}>
+                <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.one)}>
                     <CardContent>
                         <Typography color='textSecondary' gutterBottom>
                             Infected
                         </Typography>
                         <Typography variant='h5'>
-                            LIVE DATA
+                            <CountUp
+                                start={0}
+                                end={confirmed.value}
+                                duration={2.5}
+                                separator=','
+                            />
                         </Typography>
                         <Typography color="textSecondary">
-                            LIVE DATE
+                            {/* new Date().toDateString converts date to readable time */}
+                            {new Date(lastUpdate).toDateString()}
                         </Typography>
-                        <Typography>
-                            Case of COVID
+                        <Typography varient='body2'>
+                            Cases of COVID
+                        </Typography>
+
+                    </CardContent>
+                </Grid>
+                <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.two)}>
+                    <CardContent>
+                        <Typography color='textSecondary' gutterBottom>
+                            Recovered
+                        </Typography>
+                        <Typography variant='h5'>
+                        <CountUp
+                                start={0}
+                                end={recovered.value}
+                                duration={2.5}
+                                separator=','
+                            />
+                        </Typography>
+                        <Typography color="textSecondary">
+                            {new Date(lastUpdate).toDateString()}
+                        </Typography>
+                        <Typography varient='body2'>
+                            Cases of COVID
+                        </Typography>
+
+                    </CardContent>
+                </Grid>
+                <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.three)}>
+                    <CardContent>
+                        <Typography color='textSecondary' gutterBottom>
+                            Deaths
+                        </Typography>
+                        <Typography variant='h5'>
+                        <CountUp
+                                start={0}
+                                end={deaths.value}
+                                duration={2.5}
+                                separator=','
+                            />
+                        </Typography>
+                        <Typography color="textSecondary">
+                            {new Date(lastUpdate).toDateString()}
+                        </Typography>
+                        <Typography varient='body2'>
+                            Cases of COVID
                         </Typography>
 
                     </CardContent>
